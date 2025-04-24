7bcbf1b63eaf5d66445e3df54cb3d00bf3e72614
Sean980607 <a0916220504@gmail.com>
2025-04-24 11:29:11 +0800

    0

---
+++ outside/data.js
@@ -0,0 +1,227 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "圓心",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -1.076049439541972,
+        "pitch": -0.42439671468819995,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.133693330050205,
+          "pitch": 0.050586815791589856,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.5518975647529825,
+          "pitch": 0.13845796380443964,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8699709482081612,
+          "pitch": -0.6178323550919256,
+          "title": "中崙水濂洞",
+          "text": "每當下雨時這個外牆就會淋下大片雨水彷彿像是水濂洞一漾因此而得銘"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "操場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9717706615503445,
+          "pitch": -0.0007325267131967905,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.356028712717272,
+          "pitch": 0.13227323731752882,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "側門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5993300778747201,
+        "pitch": -0.28181206155401384,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6225576096018095,
+          "pitch": 0.26865826847704355,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.03551960242960561,
+          "pitch": 0.10488959532729147,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8047645504067766,
+          "pitch": -0.2551345423898539,
+          "title": "側門",
+          "text": "下課時我們通常會從側門出去因為側門離補習班比較近，而且側門有多美食可以買<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "中庭(學務處)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.354100828849802,
+          "pitch": 0.0888967626214221,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.8781107744706773,
+          "pitch": 0.20636199079606143,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "南京三民",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5757432592867735,
+          "pitch": 0.27916627575755015,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "室外",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
